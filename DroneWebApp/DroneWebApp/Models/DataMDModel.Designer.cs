c21a9b7d0723064dfaa2654d2c3ed19fd0d76785
Nathan Beyne <nathan.beyne@ugent.be>
2020-03-05 21:51:23 +0100

    Datepicker toegevoegd

--- DroneWebApp/DroneWebApp/Models/DataMDModel.Designer.cs
+++ DroneWebApp/DroneWebApp/Models/DataMDModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Eigenaar\Documents\drone1\DroneWebApp\DroneWebApp\Models\DroneDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Eigenaar\Documents\drone1\DroneWebApp\DroneWebApp\Models\DataMDModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
