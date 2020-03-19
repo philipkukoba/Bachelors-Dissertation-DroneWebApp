f604b6dab9a26e20da7a6200ca88c373e9ad6705
Bryan Van Huyneghem <bryan.vanhuyneghem@ugent.be>
2020-03-19 16:15:14 +0100

    Testing ArcGIS js API functionality & web api 2

---
+++ DroneWebApp/DroneWebApp/Controllers/CTRLPointsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DroneWebApp.Models;
+
+// TEST
+
+namespace DroneWebApp.Controllers
+{
+    public class CTRLPointsController : ApiController
+    {
+        private DroneDBEntities db = new DroneDBEntities();
+
+        // GET: api/CTRLPoints
+        public IQueryable<CTRLPoint> GetCTRLPoints()
+        {
+            return db.CTRLPoints;
+        }
+
+        // GET: api/CTRLPoints/5
+        [ResponseType(typeof(CTRLPoint))]
+        public IHttpActionResult GetCTRLPoint(int id)
+        {
+            CTRLPoint cTRLPoint = db.CTRLPoints.Find(id);
+            if (cTRLPoint == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cTRLPoint);
+        }
+
+        // PUT: api/CTRLPoints/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCTRLPoint(int id, CTRLPoint cTRLPoint)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cTRLPoint.CTRLId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cTRLPoint).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CTRLPointExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CTRLPoints
+        [ResponseType(typeof(CTRLPoint))]
+        public IHttpActionResult PostCTRLPoint(CTRLPoint cTRLPoint)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CTRLPoints.Add(cTRLPoint);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cTRLPoint.CTRLId }, cTRLPoint);
+        }
+
+        // DELETE: api/CTRLPoints/5
+        [ResponseType(typeof(CTRLPoint))]
+        public IHttpActionResult DeleteCTRLPoint(int id)
+        {
+            CTRLPoint cTRLPoint = db.CTRLPoints.Find(id);
+            if (cTRLPoint == null)
+            {
+                return NotFound();
+            }
+
+            db.CTRLPoints.Remove(cTRLPoint);
+            db.SaveChanges();
+
+            return Ok(cTRLPoint);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CTRLPointExists(int id)
+        {
+            return db.CTRLPoints.Count(e => e.CTRLId == id) > 0;
+        }
+    }
+}
--- DroneWebApp/DroneWebApp/Scripts/visualisation/world_map.js
+++ DroneWebApp/DroneWebApp/Scripts/visualisation/world_map.js
@@ -1,41 +1,92 @@
 ﻿require([
     "esri/Map",
-    "esri/views/MapView"
-], function (Map, MapView) {
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer"
+], function (Map, MapView, FeatureLayer) {
 
-    var map = new Map({
-        basemap: "topo-vector"
-    });
+        // Create the map
+        var map = new Map({
+            basemap: "topo-vector"
+        });
 
-    var view = new MapView({
-        container: "viewDiv",
-        map: map,
-        center: [3.2, 51.2],
-        zoom: 11
-    });
+        // Create the View (MapView)
+        var view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [3.2, 51.2],
+            zoom: 11
+        });
 
-    var coordsWidget = document.createElement("div");
-    coordsWidget.id = "coordsWidget";
-    coordsWidget.className = "esri-widget esri-component";
-    coordsWidget.style.padding = "7px 15px 5px";
+        // #region Example labels: trails near LA
 
-    view.ui.add(coordsWidget, "bottom-right");
+        var trailheadsRenderer = {
+            type: "simple",
+            symbol: {
+              type: "picture-marker",
+              url: "http://static.arcgis.com/images/Symbols/NPS/npsPictograph_0231b.png",
+              width: "18px",
+              height: "18px"
+            }
+        }
 
-    function showCoordinates(pt) {
-        var coords = "Lat/Lon " + pt.latitude.toFixed(3) + " " + pt.longitude.toFixed(3) +
-            " | Scale 1:" + Math.round(view.scale * 1) / 1 +
-            " | Zoom " + view.zoom;
-        coordsWidget.innerHTML = coords;
-    }
+        var trailheadsLabels = {
+            symbol: {
+              type: "text",
+              color: "#FFFFFF",
+              haloColor: "#5E8D74",
+              haloSize: "2px",
+              font: {
+                size: "12px",
+                family: "Noto Sans",
+                style: "italic",
+                weight: "normal"
+              }
+            },
+            labelPlacement: "above-center",
+            labelExpressionInfo: {
+              expression: "$feature.TRL_NAME"
+            }
+        };
 
-    view.watch("stationary", function (isStationary) {
-        showCoordinates(view.center);
-    });
 
-    view.on("pointer-move", function (evt) {
-        showCoordinates(view.toMap({ x: evt.x, y: evt.y }));
-    });
+        var trailheads = new FeatureLayer({
+            url:
+              "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
+            renderer: trailheadsRenderer,
+            labelingInfo: [trailheadsLabels]
+          });
 
+        map.add(trailheads);
+
+        //#endregion
+
+        // #region Coordinate Widget
+        // Create a coordinate widget
+        var coordsWidget = document.createElement("div");
+        coordsWidget.id = "coordsWidget";
+        coordsWidget.className = "esri-widget esri-component";
+        coordsWidget.style.padding = "7px 15px 5px";
+
+        // Add this widget to the View
+        view.ui.add(coordsWidget, "bottom-right");
+
+        // Function to show coordinates
+        function showCoordinates(pt) {
+            var coords = "Lat/Lon " + pt.latitude.toFixed(3) + " " + pt.longitude.toFixed(3) +
+                " | Scale 1:" + Math.round(view.scale * 1) / 1 +
+                " | Zoom " + view.zoom;
+            coordsWidget.innerHTML = coords;
+        }
+
+        view.watch("stationary", function (isStationary) {
+            showCoordinates(view.center);
+        });
+
+        view.on("pointer-move", function (evt) {
+            showCoordinates(view.toMap({ x: evt.x, y: evt.y }));
+        });
+
+        // #endregion
 });
 
 
