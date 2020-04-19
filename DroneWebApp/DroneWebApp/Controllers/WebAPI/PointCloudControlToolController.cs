21067855a8690a0916e9d7b32a46c0daaf161f5b
Niels Hauttekeete <niels.hauttekeete@ugent.be>
2020-04-19 14:12:16 +0200

    Script voor pointcloudControlTool

---
+++ DroneWebApp/DroneWebApp/Controllers/WebAPI/PointCloudControlToolController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DroneWebApp.Models;
+using DroneWebApp.Models.PointcloudControlTool;
+using Newtonsoft.Json;
+
+namespace DroneWebApp.Controllers.WebAPI
+{
+    public class PointCloudControlToolController : ApiController
+    {
+        private DroneDBEntities db = new DroneDBEntities();
+
+        /*
+        // GET: api/PointCloudControlTool
+        public IQueryable<PointCloudXYZ> GetPointCloudXYZs()
+        {
+            return db.PointCloudXYZs;
+        }
+        */
+
+        // GET: api/PointCloudControlTool/5
+        //[ResponseType(typeof(PointCloudXYZ))]
+        public HttpResponseMessage GetPointCloudXYZ(int id)
+        {
+            var Flight = db.DroneFlights.Find(id);
+            if (Flight == null) return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            List<PointCloudXYZ> pointCloudXYZs = Flight.PointCloudXYZs.ToList();
+            List<CTRLPoint> CTRLPoints = Flight.CTRLPoints.ToList();
+
+            Polygon polygon = new Polygon(pointCloudXYZs);
+            PointcloudControlTool tool = new PointcloudControlTool(polygon);
+
+            var list = new List<Tuple<string, bool>>().Select(t => new { CTRLName = t.Item1, Inside = t.Item2 }).ToList();
+
+            foreach (CTRLPoint ctrl in CTRLPoints)
+            {
+                bool inside = tool.PointInside3DPolygonSimplified((double)ctrl.X, (double)ctrl.Y, (double)ctrl.Z);
+                list.Add(new { ctrl.CTRLName, Inside = inside });
+            }
+
+            //config to set to json 
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(list));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return response;
+        }
+
+        /*
+        // PUT: api/PointCloudControlTool/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPointCloudXYZ(int id, PointCloudXYZ pointCloudXYZ)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pointCloudXYZ.PointCloudXYZId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(pointCloudXYZ).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PointCloudXYZExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PointCloudControlTool
+        [ResponseType(typeof(PointCloudXYZ))]
+        public IHttpActionResult PostPointCloudXYZ(PointCloudXYZ pointCloudXYZ)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PointCloudXYZs.Add(pointCloudXYZ);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = pointCloudXYZ.PointCloudXYZId }, pointCloudXYZ);
+        }
+
+        // DELETE: api/PointCloudControlTool/5
+        [ResponseType(typeof(PointCloudXYZ))]
+        public IHttpActionResult DeletePointCloudXYZ(int id)
+        {
+            PointCloudXYZ pointCloudXYZ = db.PointCloudXYZs.Find(id);
+            if (pointCloudXYZ == null)
+            {
+                return NotFound();
+            }
+
+            db.PointCloudXYZs.Remove(pointCloudXYZ);
+            db.SaveChanges();
+
+            return Ok(pointCloudXYZ);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PointCloudXYZExists(int id)
+        {
+            return db.PointCloudXYZs.Count(e => e.PointCloudXYZId == id) > 0;
+        }
+        */
+    }
+}
---
+++ DroneWebApp/DroneWebApp/Scripts/visualisation/pointcloud_control_tool.js
@@ -0,0 +1,57 @@
+﻿require([
+    "esri/Map",
+    "esri/views/MapView"
+], function (Map, MapView) {
+
+        //#region Basic setup: Map and View
+
+        // Create the map
+        let map = new Map({
+            basemap: "topo-vector"
+        });
+
+        // Create the View (MapView)
+        let view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [3.30120924, 50.85590007],
+            zoom: 20
+        });
+
+        //#endregion
+
+        //#region Visualisation Const Values 
+        //ID van de droneflight 
+        const id = $("#viewDiv").data("id");
+
+        //Lambert spacial reference (needed for all featurelayers except track visualisation)
+        const LambertSR = { wkid: 31370 };
+
+        //#endregion 
+
+        $.ajax({
+            type: "GET",
+            url: "/WebAPI/api/PointCloudControlTool/" + id, // the URL of the controller action method
+            //data: null, // optional data
+            success: (result) => {
+
+                let pointcloudControls = [];
+
+                for (let i = 0; i < result.length; i++) {
+                    pointcloudControls.push(result[i]);
+                }
+                console.log("AJAX SUCCES: PointcloudControlTool");
+
+                for (let j = 0; j < pointcloudControls.length; j++) {
+                    console.log(pointcloudControls[j].CTRLName);
+                    console.log(pointcloudControls[j].Inside);
+                }
+            },
+            error: (req, status, error) => {
+                console.log("AJAX FAIL: PointcloudControlTool");
+                console.log(req + status + error);
+            }
+        });
+
+        
+});
