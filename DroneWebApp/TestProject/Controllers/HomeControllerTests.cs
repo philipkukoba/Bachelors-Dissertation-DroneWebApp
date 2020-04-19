ba441ced69921eda9bda56f174015a3b1c97d4d0
Bryan Van Huyneghem <bryan.vanhuyneghem@ugent.be>
2020-04-19 16:39:19 +0200

    Added unit tests

---
+++ DroneWebApp/TestProject/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DroneWebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneWebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AboutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ContactTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ DroneWebApp/TestProject/Controllers/MapControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DroneWebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneWebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class MapControllerTests
+    {
+        [TestMethod()]
+        public void MapControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ViewMapTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
