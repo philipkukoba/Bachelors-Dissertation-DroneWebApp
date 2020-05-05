b74f29985d87c0343be5975e536a7c81b92a0af5
Niels Hauttekeete <niels.hauttekeete@ugent.be>
2020-05-05 08:24:42 +0200

    Update unit test

--- DroneWebApp/TestProject/Controllers/FilesControllerTests.cs
+++ DroneWebApp/TestProject/Controllers/FilesControllerTests.cs
@@ -43,7 +43,7 @@ namespace DroneWebApp.Controllers.Tests
         }
 
         [TestMethod()]
-        public void IndexTest2_ActionExecutes_ReturnsViewForIndex()
+        public void IndexTest2_Returns1ForSucces()
         {
             // Create mock context
             Mock<DroneDBEntities> mockContext = new Mock<DroneDBEntities>();
@@ -79,9 +79,9 @@ namespace DroneWebApp.Controllers.Tests
             // Set up the Find method for the mocked DbSet
             mockContext.Setup(c => c.DroneFlights.Find(It.IsAny<object[]>())).Returns((object[] input) => flights.SingleOrDefault(x => x.FlightId == (int)input.First()));            
 
-            var result = controller.Index(3, postedFiles) as ViewResult;
+            int result = controller.Index(3, postedFiles);
 
-            Assert.AreEqual("Index", result.ViewName);
+            Assert.AreEqual(1, result);
         }
 
         [TestMethod()]
@@ -110,50 +110,6 @@ namespace DroneWebApp.Controllers.Tests
             var result = controller.Export(1, "csv", "drone") as ViewResult;
 
             Assert.AreEqual("Export", result.ViewName);
-        }
-
-        [TestMethod()]
-        public void GetTotalFiles()
-        {
-            // Create mock context
-            Mock<DroneDBEntities> mockContext = new Mock<DroneDBEntities>();
-            FilesController controller = new FilesController(mockContext.Object);
-
-            string path = AppDomain.CurrentDomain.BaseDirectory + @"\testfiles";
-
-            var httpContextMock = new Mock<HttpContextBase>();
-            var serverMock = new Mock<HttpServerUtilityBase>();
-            serverMock.Setup(x => x.MapPath("~/files")).Returns(path);
-            httpContextMock.Setup(x => x.Server).Returns(serverMock.Object);
-            //httpContextMock.Setup(x => x.User.Identity.Name).Returns("testcase@user.net");
-            controller.ControllerContext = new ControllerContext(httpContextMock.Object, new RouteData(), controller);
-
-            string filePath = path + @"\harelbeke_drone_191210.csv";
-            FileStream fileStream = new FileStream(filePath, FileMode.Open);
-            int contentLength = (int)fileStream.Length;
-            fileStream.Close();
-            Mock<HttpPostedFileBase> uploadedFile = new Mock<HttpPostedFileBase>();
-            uploadedFile.Setup(x => x.FileName).Returns("harelbeke_drone_191210.csv");
-            uploadedFile.Setup(x => x.ContentType).Returns("text/csv");
-            uploadedFile.Setup(x => x.ContentLength).Returns(contentLength);
-            //uploadedFile.Setup(x => x.InputStream).Returns(fileStream);
-
-            List<HttpPostedFileBase> postedFiles = new List<HttpPostedFileBase>();
-            postedFiles.Add(uploadedFile.Object);
-
-            // Create a mock DbSet
-            List<DroneFlight> flights = GetFlights();
-            var mockSet = CreateMockSet(flights);
-            // Set up the DroneFlights property so it returns the mocked DbSet
-            mockContext.Setup(o => o.DroneFlights).Returns(() => mockSet.Object);
-            // Set up the Find method for the mocked DbSet
-            mockContext.Setup(c => c.DroneFlights.Find(It.IsAny<object[]>())).Returns((object[] input) => flights.SingleOrDefault(x => x.FlightId == (int)input.First()));
-
-            var result = controller.Index(3, postedFiles) as ViewResult;
-
-            int totalFiles = controller.GetTotalFiles();
-
-            Assert.AreEqual(1, totalFiles);
         }
 
         [TestMethod()]
@@ -199,50 +155,6 @@ namespace DroneWebApp.Controllers.Tests
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(JsonResult));
-        }
-
-        [TestMethod()]
-        public void GetResultAndFileNameTest()
-        {
-            // Create mock context
-            Mock<DroneDBEntities> mockContext = new Mock<DroneDBEntities>();
-            FilesController controller = new FilesController(mockContext.Object);
-
-            string path = AppDomain.CurrentDomain.BaseDirectory + @"\testfiles";
-
-            var httpContextMock = new Mock<HttpContextBase>();
-            var serverMock = new Mock<HttpServerUtilityBase>();
-            serverMock.Setup(x => x.MapPath("~/files")).Returns(path);
-            httpContextMock.Setup(x => x.Server).Returns(serverMock.Object);
-            //httpContextMock.Setup(x => x.User.Identity.Name).Returns("testcase@user.net");
-            controller.ControllerContext = new ControllerContext(httpContextMock.Object, new RouteData(), controller);
-
-            string filePath = path + @"\harelbeke_drone_191210.csv";
-            FileStream fileStream = new FileStream(filePath, FileMode.Open);
-            int contentLength = (int)fileStream.Length;
-            fileStream.Close();
-            Mock<HttpPostedFileBase> uploadedFile = new Mock<HttpPostedFileBase>();
-            uploadedFile.Setup(x => x.FileName).Returns("harelbeke_drone_191210.csv");
-            uploadedFile.Setup(x => x.ContentType).Returns("text/csv");
-            uploadedFile.Setup(x => x.ContentLength).Returns(contentLength);
-            //uploadedFile.Setup(x => x.InputStream).Returns(fileStream);
-
-            List<HttpPostedFileBase> postedFiles = new List<HttpPostedFileBase>();
-            postedFiles.Add(uploadedFile.Object);
-
-            // Create a mock DbSet
-            List<DroneFlight> flights = GetFlights();
-            var mockSet = CreateMockSet(flights);
-            // Set up the DroneFlights property so it returns the mocked DbSet
-            mockContext.Setup(o => o.DroneFlights).Returns(() => mockSet.Object);
-            // Set up the Find method for the mocked DbSet
-            mockContext.Setup(c => c.DroneFlights.Find(It.IsAny<object[]>())).Returns((object[] input) => flights.SingleOrDefault(x => x.FlightId == (int)input.First()));
-
-            controller.Index(3, postedFiles);
-
-            var result = controller.GetResultsAndFileNames();
-
-            Assert.IsNotNull(result);
         }
 
         private List<Drone> GetDrones()
