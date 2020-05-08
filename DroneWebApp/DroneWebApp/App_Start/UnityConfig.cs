e03e8cd7a36f5373ede7af44453d78b464ac8a02
Niels Hauttekeete <niels.hauttekeete@ugent.be>
2020-05-08 14:40:01 +0200

    Register en login toegevoegd

--- DroneWebApp/DroneWebApp/App_Start/UnityConfig.cs
+++ DroneWebApp/DroneWebApp/App_Start/UnityConfig.cs
@@ -1,7 +1,14 @@
+using DroneWebApp.Controllers;
 using DroneWebApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System.Data.Entity;
+using System.Web;
 using System.Web.Mvc;
 using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
 using Unity.Mvc5;
 
 namespace DroneWebApp
@@ -17,8 +24,34 @@ namespace DroneWebApp
 
             // e.g. container.RegisterType<ITestService, TestService>();
             // Register the database and let Unity manage it for Dependency Injection
-            container.RegisterType<DbContext, DroneDBEntities > (); // added
+            container.RegisterType<DbContext, DroneDBEntities > ("DroneDBEntities"); // added
+
+            container.RegisterType<IdentityDbContext<ApplicationUser>, ApplicationDbContext>("ApplicationDbContext", new HierarchicalLifetimeManager());
+            container.RegisterType<UserManager<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(new HierarchicalLifetimeManager());
+            container.RegisterType<AccountController>(new InjectionConstructor());
+            container.RegisterType<ManageController>(new InjectionConstructor());
+            container.RegisterType<IAuthenticationManager>(new InjectionFactory(o => HttpContext.Current.GetOwinContext().Authentication));
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            container.RegisterType<DroneFlightsController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            container.RegisterType<DronesController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            container.RegisterType<FilesController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            container.RegisterType<MapController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            container.RegisterType<PilotsController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            container.RegisterType<ProjectsController>(new InjectionConstructor(new ResolvedParameter<DbContext>("DroneDBEntities")));
+            //container.RegisterType<AccountController>(new InjectionConstructor(new ResolvedParameter<DbContext>("ApplicationDbContext")));
+            //container.RegisterType<ManageController>(new InjectionConstructor(new ResolvedParameter<DbContext>("ApplicationDbContext")));
+
+            DroneFlightsController droneFlightsController = container.Resolve<DroneFlightsController>();
+            DronesController dronesController = container.Resolve<DronesController>();
+            FilesController filesController = container.Resolve<FilesController>();
+            MapController mapController = container.Resolve<MapController>();
+            PilotsController pilotsController = container.Resolve<PilotsController>();
+            ProjectsController projectsController = container.Resolve<ProjectsController>();
+            AccountController accountController = container.Resolve<AccountController>();
+            ManageController manageController = container.Resolve<ManageController>();
         }
     }
 }
--- DroneWebApp/DroneWebApp/Models/DroneDBModel.Context.cs
+++ DroneWebApp/DroneWebApp/Models/DroneDBModel.Context.cs
@@ -26,6 +26,10 @@ namespace DroneWebApp.Models
         }
     
         public virtual DbSet<AbsoluteGeolocationVariance> AbsoluteGeolocationVariances { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<CTRLPoint> CTRLPoints { get; set; }
         public virtual DbSet<DepartureInfo> DepartureInfoes { get; set; }
         public virtual DbSet<DestinationInfo> DestinationInfoes { get; set; }
